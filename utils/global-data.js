fe74856f54b1c70faebd3e4d58ee1e1ed20921c9
aproinov <98790856+aproinov@users.noreply.github.com>
2024-12-09 10:08:01 +0700

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'MSarif';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'UAE Time News';
+    : 'Teknosia';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
