0c05a573f6fe50d699dd5a357773254ab75238ce
aproinov <98790856+aproinov@users.noreply.github.com>
2024-12-10 21:06:15 +0700

    Update SEO.js

--- components/SEO.js
+++ components/SEO.js
@@ -6,6 +6,7 @@ export default function SEO({ title, description }) {
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
+    <meta name="google-site-verification" content="iv48T9CfZfs6q8Cnlqy8uZmxBw75tt2MC4M2DvA8Z38" />
     </Head>
   );
 }
